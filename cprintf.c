3eb3ec4ba0eb3882bf1dac1df8fead881992713b
Barry <rountree4@llnl.gov>
2022-09-09 12:51:34 -0700

    Complete rework version #7 or so.

--- cprintf.c
+++ cprintf.c
@@ -4,7 +4,30 @@
 #include <string.h>     // strncmp
 #include <stdlib.h>     // calloc
 #include <stdio.h>      // fprintf
+#include <string.h>     // strspan
 #include "cprintf.h"
+
+struct atom{
+    bool populated;
+    bool is_conversion_specification;
+    size_t original_field_width;
+    size_t new_field_width;
+
+    char *original_specification;
+    char *new_specification;
+    char *flags;
+    char *field_width;
+    char *precision;
+    char *length_modifier;
+    char *conversion_specifier;
+    char *ordinary_text;
+    
+    // navigation
+    struct atom *right;
+    struct atom *left;
+    struct atom *up;
+    struct atom *down;
+};
 
 static struct atom * origin = NULL;
 
@@ -54,7 +77,6 @@ free_graph( struct atom *a ){
         a->left->right = NULL;
     }
     free( a );
-    fprintf( stdout, "%s:%d node %p freed\n", __FILE__, __LINE__, a );
     return;
 }
 
@@ -94,27 +116,128 @@ create_atom( bool is_newline ){
     return last_populated_atom;
 }
 
-bool
-ischartoken( const char * const restrict p, const char c, const char ** restrict q ){
-    assert(p != NULL);
-    bool rc = (*p == c);
-    if( q != NULL ){
-        // aka if(rc == true){ *q = p+sizeof(char); }else{ *q = p; }
-        *q = p + rc;    
-    }
-    return rc;
+
+// Conversion specifications look like this:
+// %[flags][field_width][.precision][length_modifier]specifier
+
+
+ptrdiff_t
+parse_flags( const char *p ){
+    // Returns the number of bytes starting from the beginning
+    // of p that consist only of the characters #0- +'I
+    return strspn( p, "#0- +'I" );
 }
 
-bool
-isstrtoken( const char * const restrict p, const char * const restrict s, const char ** restrict q ){
-    assert( p != NULL );
-    assert( s != NULL );
-    bool rc = ! strncmp( p, s, strlen(s) );
-    if( q != NULL ){
-        // aka if(rc == true){ *q = p + strlen(s); }else{ *q = p; }
-        *q = p + (rc * strlen(s));
-    }
-    return rc;
+ptrdiff_t
+parse_field_width( const char *p ){
+    // Returns the number of bytes in the field width,
+    // or zero of no field width specified.
+    char *end;
+    assert( '*' != *p );    // Don't support * or *n$ (yet).
+    strtol( p, &end, 10 );
+    return end - p;
 }
+
+ptrdiff_t
+parse_precision( const char *p ){
+    char *end;
+    // Returns the number of bytes representing
+    // precision, including the leading '.'.  
+    if( '.' == *p ){
+        assert( '*' != *(p+1) ); // Don't support * or *n$ (yet).
+        strtol( p+1, &end, 10 );
+        return (end - p); 
+    }else{
+        return 0;
+    }
+}
+
+ptrdiff_t 
+parse_length_modifier( const char *p ){
+    // length modifiers are:
+    // h, hh, l, ll, L, q, j, z, t
+    return strspn( p, "hlLqjzt" );
+}
+
+ptrdiff_t 
+parse_conversion_specifier( const char *p ){
+    // conversion specifiers are:
+    // d, i, o, u, x, X, e, E, f, F, g, G, a, A, c, C, s, S, p, n, m, %
+    size_t d = strspn( p, "diouxXeEfFgGaAcCsSpnm%" );
+    // This one is mandatory and there can only be one.
+    assert( d==1 );
+    return d;
+}
+
+void
+archive( const char *p, ptrdiff_t span, char **q ){
+    static char *empty = "";
+    if( 0 == span ){
+        *q = empty;
+    }else{
+        *q = calloc( span+1, 1 );
+        strncpy( *q, p, span );
+    }
+}
+
+void
+cprintf( const char *fmt, ... ){
+    struct atom *a;
+    const char *p = fmt, *q = fmt;
+    ptrdiff_t d = 0;
+    ptrdiff_t span;
+    while( *p != '\0' ){
+        d = strcspn( p, "%" ); 
+        q = p;
+        if( d == 0 ){
+            // We've found a converstion specification.
+            a = create_atom( false );
+            a->is_conversion_specification = true;
+
+            q++; // Skip over initial '%'
+            fprintf( stdout, "%s:%d *q = '%c'\n", __FILE__, __LINE__, *q );
+
+            span = parse_flags( q );
+            archive( q, span, &(a->flags) );
+            q += span;
+            fprintf( stdout, "%s:%d a->flags=\"%s\"\n", __FILE__, __LINE__, a->flags );
+            
+            span = parse_field_width( q );
+            archive( q, span, &(a->field_width) );
+            q += span;
+            fprintf( stdout, "%s:%d a->field_width=\"%s\"\n", __FILE__, __LINE__, a->field_width );
+
+            span = parse_precision( q );
+            archive( q, span, &(a->precision) );
+            q += span;
+            fprintf( stdout, "%s:%d a->precision=\"%s\"\n", __FILE__, __LINE__, a->precision );
+
+            span = parse_length_modifier( q );
+            archive( q, span, &(a->length_modifier) );
+            q += span;
+            fprintf( stdout, "%s:%d a->length_modifier=\"%s\"\n", __FILE__, __LINE__, a->length_modifier );
+
+            span = parse_conversion_specifier( q );
+            archive( q, span, &(a->conversion_specifier) );
+            q += span;
+            fprintf( stdout, "%s:%d a->conversion_specifier=\"%s\"\n", __FILE__, __LINE__, a->conversion_specifier );
+
+            archive( p, (q-p)+1, &(a->original_specification) ); 
+            fprintf( stdout, "%s:%d a->original_specification=\"%s\"\n", __FILE__, __LINE__, a->original_specification );
+            fprintf( stdout, "\n");
+
+            p = q;
+        }else{
+            // We've found some normal text.
+            a = create_atom( false );
+            a->is_conversion_specification = false;
+            archive( q, d, &(a->ordinary_text) );
+            q += d;
+            fprintf( stdout, "%s:%d a->ordinary_text=\"%s\"\n", __FILE__, __LINE__, a->ordinary_text );
+            p = q;
+        }
+    }
+}
+
 
 
--- harness.c
+++ harness.c
@@ -3,107 +3,14 @@
 #include <stdbool.h>
 #include "cprintf.h"
 
-
-void
-ischartoken_test(){
-    const char * const restrict p = "Hello, world!\n";
-    const char * q = NULL;
-    bool rc;
-
-    q = NULL;
-    rc = ischartoken( p, 'H', &q );
-    assert( rc == true );
-    assert( q == p+1 );
-
-    q = NULL;
-    rc = ischartoken( p, 'e', &q );
-    assert( rc == false );
-    assert( q == p );
-
-    q = NULL;
-    rc = ischartoken( p, 'H', NULL );
-    assert( rc == true );
-    assert( q == NULL );
-
-    q = NULL;
-    rc = ischartoken( p, 'e', NULL );
-    assert( rc == false );
-    assert( q == NULL );
-}
-
-void
-isstrtoken_test(){
-    const char * const restrict p = "Hello, world!\n";
-    const char * q = NULL;
-    bool rc;
-
-    q = NULL;
-    rc = isstrtoken( p+2, "ll", &q );
-    assert( rc == true );
-    assert( q == p+4 );
-
-    q = NULL;
-    rc = isstrtoken( p+2, "qq", &q );
-    assert( rc == false );
-    assert( q == p+2 );
-
-    q = NULL;
-    q = NULL;
-    rc = isstrtoken( p+2, "ll", NULL );
-    assert( rc == true );
-    assert( q == NULL );
-
-    q = NULL;
-    rc = isstrtoken( p+2, "qq", NULL );
-    assert( rc == false );
-    assert( q == NULL );
-}
-
-void
-create_atom_test(){
-    struct atom *a;
-    /* 11 21 31
-     * 12 22 32
-     * 13 23 33
-     */
-    for( int i = 1; i<4; i++ ){
-        for( int j = 10; j<40; j=j+10 ){
-            if( j == 10 ){
-                a = create_atom( true );
-            }else{
-                a = create_atom( false );
-            }
-            a->new_field_width = i+j;
-        }
-    }
-    //dump_graph();
-    assert( a->new_field_width );
-    assert( 33 == a->new_field_width );
-    assert( a->left );
-    assert( a->left->new_field_width );
-    assert( 23 == a->left->new_field_width );
-    assert( a->left->up );
-    assert( a->left->up->new_field_width );
-    assert( 22 == a->left->up->new_field_width );
-    assert( a->left->up->left );
-    assert( a->left->up->left->new_field_width );
-    assert( 12 == a->left->up->left->new_field_width );
-    assert( a->left->up->left->up );
-    assert( a->left->up->left->up->new_field_width );
-    assert( 11 == a->left->up->left->up->new_field_width );
-    assert( a->left->up->left->up->right );
-    assert( a->left->up->left->up->right->new_field_width );
-    assert( 21 == a->left->up->left->up->right->new_field_width );
-    assert( a->left->up->left->up->right->down );
-    assert( a->left->up->left->up->right->down->new_field_width );
-    assert( 22 == a->left->up->left->up->right->down->new_field_width );
-    free_graph( NULL );
-}
-
 int main(){
-    ischartoken_test();
-    isstrtoken_test();
-    create_atom_test();   
+    cprintf("foo");
+    cprintf("foo\n");
+    cprintf("%s");
+    cprintf("%#x");
+    cprintf("%#30x");
+    cprintf("%#030.7f");
+    cprintf("%#030.7llf");
     return 0;
 
 }
