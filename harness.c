00c4456687af9e6bda43a32c23cb74914d79ad4e
Barry <rountree4@llnl.gov>
2022-09-11 19:25:37 -0700

    Working well.  Let's call it a night.

--- harness.c
+++ harness.c
@@ -8,21 +8,26 @@ int main(){
     cprintf("%d %d %d\n", 10, 20, 30);
     cprintf("%d %d %d\n", 100, 200, 300);
     cflush();
-    dump_graph();
     free_graph();
 
     printf("============\n");
-    fflush(NULL);
 
     cprintf("a=%07.4f b= %07.5Lf\n", 1.2, 1.2L );
-    /*
-    cprintf("a=%07.4lf b= %07.5lf\n", 10.22, 10.22L );
-    cprintf("a=%07.4lf b= %07.5lf\n", 100.222, 100.222L );
-    cprintf("a=%07.4lf b= %07.5lf\n", 1000.2222, 1000.2222L );
-    */
+    cprintf("a=%07.4lf b= %07.5Lf\n", 10.22, 10.22L );
+    cprintf("a=%07.4lf b= %07.5Lf\n", 100.222, 100.222L );
+    cprintf("a=%07.4lf b= %07.5Lf\n", 1000.2222, 1000.2222L );
     cflush();
-    dump_graph();
     free_graph();
+
+    printf("============\n");
+
+    cprintf("%s | %s | %s | %-s \n", "tiny", "longer", "pretty long", "downright wordy");
+    cprintf("%0d | %.2f | %p | %-c \n", 0, 3.14, main, 'x');
+    cprintf("%0d | %.2f | %p | %-c \n", 20000, 300000.14, cprintf, 'y');
+    cprintf("%0d | %.2f | %p | %-c \n", 20, 30.14, printf, 'z');
+    cflush();
+    free_graph();
+
     return 0;
 }
 
